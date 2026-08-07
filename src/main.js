8fc9d46ce4529a81f28ac41d61d7c551f146edf5
Oindil Golder <oindilgolder303@gmail.com>
2026-08-07 19:08:24 +0530

    6 aug 2026

--- src/main.js
+++ src/main.js
@@ -1,60 +1,57 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+// Dom selected elements
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const domElements = {
+  playground: document.getElementById("playground"),
+  newCardsWrapper: document.getElementById("new-cards")
+}
 
-<div class="ticks"></div>
+// style
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+const styles = {
+  newElementCard: "h-fit text-gray w-fit px-5 py-2 border border-zinc-300 bg-linear-to-t from-sky-100 to-sky-200 rounded cursor-pointer hover:scale-105 hover:shadow transition ease-linear duration-75"
+}
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+// logic
 
-setupCounter(document.querySelector('#counter'))
+const defaultCardMap = new Map([
+  ["🔥 Fire", "🔥 Fire"],
+  ["🌱 Earth", "🌱 Earth"],
+  ["💦 Water", "💦 Water"],
+  ["💨 Air", "💨 Air"],
+])
+
+// console.log(defaultCardMap);
+
+/**
+ * @param {string} key 
+ * @param {string} value 
+ */
+function createNewElement(key, value) {
+  const divElement = document.createElement("div");
+  divElement.className = styles.newElementCard;
+  divElement.textContent = value;
+  divElement.id = key;
+
+  divElement.addEventListener("click", () => {
+
+  })
+
+
+  return divElement;
+}
+
+
+function renderNewCards() {
+  domElements.newCardsWrapper.innerHTML = "";
+  const cards = [];
+
+  defaultCardMap.forEach((value, key) => {
+    cards.push(createNewElement(key, value))
+  })
+
+  domElements.newCardsWrapper.append(...cards);
+
+}
+
+
+renderNewCards();
